0ae01e6e6b938111e9b7426c3eb9b94f52691d41
SOHTA0318 <nicosohta0318@gmail.com>
2022-03-02 20:02:37 -0800

    first commit

---
+++ demo/variables.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  
+  $name = 'Edwin'; 
+  $number = 100; 
+  $numberList = 100;
+  $Number_List = 100;
+  // $number-list = 100;
+  // $0number = 100;
+  $NUMBER = 100;
+  $NUMBeR = '100';
+
+  echo $name . ' ' . $NUMBeR;
+
+  $Name = '<h1>Hello</h1>';
+  echo $Name;
+  
+  ?>
+</body>
+
+</html>
