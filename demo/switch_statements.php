386dc6769286fa713d238e6873f6b67d2b9179a3
SOHTA0318 <nicosohta0318@gmail.com>
2022-03-04 12:12:34 -0800

    if & switch

---
+++ demo/switch_statements.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php 
+
+  $number = 4;
+  // if($number <10){
+  //   echo 'This';
+  // }
+  // if($number <20){
+  //   echo 'This';
+  // }
+  // if($number <30){
+  //   echo 'This';
+  // }
+  switch($number){
+    case 34:
+    echo 'it is 34';
+    break;
+    case 37:
+    echo 'it is 34';
+    break;
+    case 40:
+    echo 'it is 34';
+    break;
+    default:
+    echo 'We could not find anything';
+  }
+  ?>
+</body>
+
+</html>
