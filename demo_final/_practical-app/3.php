9a34cdebd3fa6cae7215777a552fc3d964c02bb7
SOHTA0318 <nicosohta0318@gmail.com>
2022-03-04 20:57:22 -0800

    foreach & assignment3

--- demo_final/_practical-app/3.php
+++ demo_final/_practical-app/3.php
@@ -1,19 +1,49 @@
 <?php include "functions.php" ?>
 <?php include "includes/header.php" ?>
 
-	<section class="content">
+<section class="content">
 
-	<aside class="col-xs-4">
+  <aside class="col-xs-4">
 
-	<?php Navigation();?>
-			
-	</aside><!--SIDEBAR-->
+    <?php Navigation();?>
+
+  </aside>
+  <!--SIDEBAR-->
 
 
-<article class="main-content col-xs-8">
+  <article class="main-content col-xs-8">
 
-<?php  
+    <?php  
 
+if(false){
+	echo 'It is not right';
+}elseif(false){
+	echo 'It is not right';
+}else{
+	echo 'I love PHP';
+}
+
+for ($num = 0; $num <10; $num++){
+	echo $num. '<br>';
+}
+
+$test = 3;
+switch($test){
+	case 4:
+		echo 'it is four';
+		break;
+	case 5:
+		echo 'it is five';
+		break;
+	case 3:
+		echo 'it is three';
+		break;
+	case 2:
+		echo 'it is two';
+		break;
+		default:
+		echo 'it is default';
+}
 /*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
 
 
@@ -33,6 +63,7 @@
 
 
 
-</article><!--MAIN CONTENT-->
-	
-<?php include "includes/footer.php" ?>
+  </article>
+  <!--MAIN CONTENT-->
+
+  <?php include "includes/footer.php" ?>
