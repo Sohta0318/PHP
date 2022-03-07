daafa31ec696e0254c8161b55295ce69188f4fa2
SOHTA0318 <nicosohta0318@gmail.com>
2022-03-07 14:43:02 -0800

    read data from sql

---
+++ demo/mysql/functions.php
@@ -0,0 +1,20 @@
+<?php include "db.php";?>
+<?php ;
+
+
+function showAllData(){
+  global $connection;
+$query = "SELECT * FROM users";
+
+$result = mysqli_query($connection, $query);
+
+if(!$result){
+  die("QUERY FAILED". mysqli_error($connection));
+}
+
+while($row = mysqli_fetch_assoc($result)){
+  $id = $row['id'];
+  echo "<option value='$id'>$id</option>";
+}
+}
+?>
