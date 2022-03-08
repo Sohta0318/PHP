b67a76daefcd67f3dea21cd3bc4fe9ab7b4c5c15
SOHTA0318 <nicosohta0318@gmail.com>
2022-03-08 14:37:45 -0800

    delete from sql & assignment 7

--- demo_final/_practical-app/7.php
+++ demo_final/_practical-app/7.php
@@ -1,22 +1,47 @@
 <?php include "functions.php" ?>
 <?php include "includes/header.php" ?>
-    
 
-	<section class="content">
 
-		<aside class="col-xs-4">
+<section class="content">
 
-		<?php Navigation();?>
+  <aside class="col-xs-4">
+
+    <?php Navigation();?>
+
+
+  </aside>
+  <!--SIDEBAR-->
+
+
+  <article class="main-content col-xs-8">
+
+
+    <?php  
+    $connection =mysqli_connect('localhost','root','','assignment7');
+		if(!$connection){
+			echo 'Connect failed';
+		}
+
+		if(isset($_POST['submit'])){
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			$query = 'INSERT INTO test (username, password) ';
+			$query .= "VALUES ('$username', $password)";
+
+			
+
+			$result = mysqli_query($connection,$query);
 			
 			
-		</aside><!--SIDEBAR-->
+			if($result){
+				echo 'Successfully submitted!';
+			}else{
+				die('Something went wrong '. mysqli_error($connection));
+			}
+		}
+		$read = "SELECT * FROM test";
+		$result2 = mysqli_query($connection,$read);
 
-
-	<article class="main-content col-xs-8">
-	
-	
-	
-	<?php  
 
 	/*  Step 1 - Create a database in PHPmyadmin
 
@@ -29,11 +54,29 @@
 */
 	
 	?>
+    <form action="7.php" method="post">
+      <label for="username">
+        <input type="text" name="username" id="username">
+      </label>
+      <label for="password">
+        <input type="password" name="password" id="password">
+      </label>
+      <input type="submit" value="Submit" name="submit">
+    </form>
+
+    <?php 
+		global $result2;
+		while($row = mysqli_fetch_assoc($result2)){
+			foreach ($row as $key => $value) {
+			echo "<p>$key: $value</p>";
+		}
+		}
+		?>
 
 
 
 
+  </article>
+  <!--MAIN CONTENT-->
 
-</article><!--MAIN CONTENT-->
-
-<?php include "includes/footer.php" ?>
+  <?php include "includes/footer.php" ?>
