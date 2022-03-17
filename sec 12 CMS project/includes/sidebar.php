3b646e11a3cec7d26c5f86da86a4e7b70431aa8f
SOHTA0318 <nicosohta0318@gmail.com>
2022-03-17 03:17:31 -0700

    login & logout

--- sec 12 CMS project/includes/sidebar.php
+++ sec 12 CMS project/includes/sidebar.php
@@ -17,6 +17,22 @@
     <!-- /.input-group -->
   </div>
 
+  <!-- Login -->
+  <div class="well">
+    <form action="includes/login.php" method="post">
+      <h4>Login</h4>
+      <div class="form-group">
+        <input name="username" type="text" class="form-control" placeholder="Enter Username">
+      </div>
+      <div class="input-group">
+        <input name="password" type="password" class="form-control" placeholder="Enter Password">
+        <span class="input-group-btn"><button class="btn btn-primary" name="login" type="submit">Login</button></span>
+      </div>
+    </form>
+    <!--search form -->
+    <!-- /.input-group -->
+  </div>
+
   <!-- Blog Categories Well -->
   <div class="well">
 
