2f0d8679fce8bd05ed54abae4af3d0fdca4d097e
SOHTA0318 <nicosohta0318@gmail.com>
2022-03-22 17:43:42 +0900

    fix to if

--- 4-1~4/01_php_base/lesson10.php
+++ 4-1~4/01_php_base/lesson10.php
@@ -12,20 +12,15 @@
 $score = array(10,60,90,70,50);
 
 for ($i=0; $i < sizeof($score); $i++) { 
-  switch ($score[$i]) {
-    case $score[$i] >= 80:
-      echo "<p>優</p>";
-      break;
-    case $score[$i] >= 60:
-      echo "<p>良</p>";
-      break;
-    case $score[$i] >= 40:
-      echo "<p>可</p>";
-      break;
-    
-    default:
+
+  if($score[$i] >= 80){
+    echo "<p>優</p>";
+  }elseif($score[$i] >= 60){
+    echo "<p>良</p>";
+  }elseif($score[$i] >= 40){
+    echo "<p>可</p>";
+  }else{
     echo "<p>不可</p>";
-      break;
   }
 }
 
